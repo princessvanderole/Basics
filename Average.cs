21ad9443f1e80af17922486be20e30d1a322a61a
princessvanderole <152167362+princessvanderole@users.noreply.github.com>
2023-11-29 15:21:21 +0100

    Durschnittsberechnung
    Zusatzaufgabe Taschenrechner

---
+++ Average.cs
@@ -0,0 +1,40 @@
+/*using System;
+
+namespace Calculator
+{
+    class Program2
+    {
+
+        static void Main(string[] args)
+        {
+
+            double num01;
+            double num02;
+            double num03;
+
+            Console.WriteLine ("Input a number: ");
+            num01 = Convert.ToDouble (Console.ReadLine() );
+
+
+            Console.WriteLine ("Input number 2: ");
+            num02 = Convert.ToDouble (Console.ReadLine() );
+
+
+            Console.WriteLine ("Input number 3: ");
+            num03 = Convert.ToDouble (Console.ReadLine() );
+
+
+            double result = (num01 + num02 + num03) /3;
+
+            Console.WriteLine("The average of these numbers is: " + result);
+
+            Console.ReadKey();
+
+
+
+        }
+
+    }
+
+
+}*/
