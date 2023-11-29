288759f6e7aa1612207c379646e172a147b571cb
princessvanderole <152167362+princessvanderole@users.noreply.github.com>
2023-11-29 15:20:26 +0100

    Variablen erstellen
    erstellen von Variablen und Werte Zuweisung

---
+++ Variablen.cs
@@ -0,0 +1,51 @@
+/*using System;
+
+namespace My_second_Awesome_Program
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            //creating a variable aber keinen wert assignen
+
+            //int num01;
+            //int num02;
+            //float num01;//=kommazahl
+            //float num02;
+            
+            double num01;//=zahl mit mehreren nachkommastellen
+            double num02;
+
+            Console.Write("Input a Number: ");
+            // theoretisch wird hier ein string eingegeben 
+            //dieser muss in eine int convertiert werden
+            //die convertierung muss in der Klammer (funktion)die consolen eingabe einthalten
+            //num01 = Convert.ToInt32( Console.ReadLine() ); 
+            // die 32 gibt die Zahlengröße in Bit an, es gibt auch noch 16 oder 64 für große zahlen
+
+
+            num01 = Convert.ToDouble( Console.ReadLine() ); 
+            Console.Write("Input a second number: ");
+
+            //num02 = Convert.ToInt32 (Console.ReadLine());
+
+            num02 = Convert.ToDouble( Console.ReadLine() ); 
+
+            //int result = num01 * num02;
+            // die integer result(ergebnis) ist die summe aus 01 mal 02, wird somit als int ausgegeben
+
+            double result = num01 / num02;//zahlen durch geteilt
+            Console.WriteLine("The result is " + result);
+            
+            //Console.WriteLine("Whats your name?");
+
+            //string userName = Console.ReadLine();
+
+            //Console.WriteLine("Hello " + userName + ", nice to meet you!");
+
+            //wait befor close
+            Console.ReadKey();
+
+        }
+    }
+}*/
